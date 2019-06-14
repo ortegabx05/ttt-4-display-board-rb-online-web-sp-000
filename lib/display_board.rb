def76a91527f55f85e3770007a7d389fd310c412
jessi ortega <ortegabx05@gmail.com>
2019-06-14 18:53:56 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 def display_board(board=[" "," "," "," "," "," "," "," "," "])
   puts "   |   |   "
   puts "-----------"
-  puts "   | X |   "
+  puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
